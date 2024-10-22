f18905f861c945768142703d9e1a5f3187513aac
Chris Lowen <chrislowen@pm.me>
2024-10-22 15:15:09 -0400

    begin implementing mandelbrot example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,94 @@
+// TODO - implement Complex myself for practice
+use num::Complex;
+use std::str::FromStr;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        // z diverges if its radius is greater than 2
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c
+    }
+    None
+}
+
+/// Given the row nd column of a  pixel in the output image, return the corresponding point on the complex plane
+///
+/// `bounds` is a pair giving the width and height of the image in pixels
+/// `pixel` is a (column, row) pair indicating a particular pixel in that image
+/// The `upper_left` and `lower_right` parameters are points on the complex plane designating the area our image covers
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im + pixel.1 as f64 * height / bounds.1 as f64,
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 200),
+            (25, 175),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
+        Complex {
+            re: -0.5,
+            im: -0.75
+        }
+    )
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        _ => None,
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("-1.2,0.1"),
+        Some(Complex { re: -1.2, im: 0.1 })
+    );
+    assert_eq!(parse_complex("-1.234,dklfjd"), None);
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20noise", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.2", 'x'), Some((0.5, 1.2)));
 }
