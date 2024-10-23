35647b06fe46ce87f764ae82b144d7917ed700be
Chris Lowen <chrislowen@pm.me>
2024-10-23 11:44:33 -0400

    write png files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 // TODO - implement Complex myself for practice
+use image::codecs::png::PngEncoder;
+use image::{ExtendedColorType, ImageEncoder};
 use num::Complex;
+use std::env;
+use std::fs::File;
 use std::str::FromStr;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png, 1000x750, -1.20,0.35 -1,0.20", args[0]);
+        std::process::exit(1);
+    }
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower_right corner point");
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+}
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> image::error::ImageResult<()> {
+    let output = File::create(filename)?;
+    let encoder = PngEncoder::new(output);
+    let _ = encoder.write_image(pixels, bounds.0 as u32, bounds.1 as u32, ExtendedColorType::L8);
+    Ok(())
 }
 
 /// Render a rectangle of the fractal into a buffer of pixels
@@ -11,12 +35,13 @@ fn main() {
 /// The `bounds` argument gives the width and height of the buffer in `pixels`, which holds one grayscale pixel per byte.
 /// The `upper_left` and `lower_right` arguments specify points on the complex plane which corresponde to
 /// the upper-left and lower-right corners of the pixel buffer.
-fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, upper_right: Complex<f64>) {
+fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) {
     assert!(pixels.len() == bounds.0 * bounds.1);
-    for row in 0..bounds.0 {
-        for col in 0..bounds.1 {
-            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
-            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+
+    for row in 0..bounds.1 {
+        for col in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (col, row), upper_left, lower_right);
+            pixels[row * bounds.0 + col] = match escape_time(point, 255) {
                 None => 0,
                 Some(count) => 255 - count as u8,
             };
