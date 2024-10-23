27fc1b58482180ef612fbbc31e1b08549ce2d678
Chris Lowen <chrislowen@pm.me>
2024-10-23 10:54:40 -0400

    add render function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,24 @@ fn main() {
     println!("Hello, world!");
 }
 
+/// Render a rectangle of the fractal into a buffer of pixels
+///
+/// The `bounds` argument gives the width and height of the buffer in `pixels`, which holds one grayscale pixel per byte.
+/// The `upper_left` and `lower_right` arguments specify points on the complex plane which corresponde to
+/// the upper-left and lower-right corners of the pixel buffer.
+fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, upper_right: Complex<f64>) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+    for row in 0..bounds.0 {
+        for col in 0..bounds.1 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
@@ -18,7 +36,7 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     None
 }
 
-/// Given the row nd column of a  pixel in the output image, return the corresponding point on the complex plane
+/// Given the row and column of a  pixel in the output image, return the corresponding point on the complex plane
 ///
 /// `bounds` is a pair giving the width and height of the image in pixels
 /// `pixel` is a (column, row) pair indicating a particular pixel in that image
